f24a8daa38ad1d96887e61cb2e0dd32c96147343
Hamza BENDALI BRAHAM <hbendali@ya.ru>
2021-05-21 10:12:11 +0100

    up

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  freq:2400,                            // in [MHz] 2.4GHz
-  elements:15,                          // 3..22 elements
-  diameter_of_parasitic_elements:4,     // in [mm]
+  freq:2450,                            // in [MHz] 2.4GHz
+  elements:20,                          // 3..22 elements
+  diameter_of_parasitic_elements:2,     // in [mm]
   diameter_of_boom:20,                  // in [mm]
   boom_isolated:true,                   // true, false
 }
